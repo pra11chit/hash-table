0422f60ea7d779a8583d0978146b80500dcfee45
pra11chit <pratikchitode@gmail.com>
2020-09-20 16:17:40 +0530

    initial commit

---
+++ src/hash_table.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+    ht->count = 0;
+    ht->size = 53;
+    ht->items = calloc(ht->count, sizeof(ht_item*));
+    return ht;
+}
